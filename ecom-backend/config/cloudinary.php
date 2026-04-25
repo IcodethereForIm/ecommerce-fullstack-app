9620241e6c8c8a31c5756ff1f3664f16c9f15cc9
Souvik Ghosh <personalpiku61140@gmail.com>
2026-04-26 01:05:22 +0530

    fixing some clodinary isuue

--- ecom-backend/config/cloudinary.php
+++ ecom-backend/config/cloudinary.php
@@ -4,8 +4,8 @@ return [
 
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
 
-    'api_key' => env('CLOUDINARY_KEY'),
+    'api_key' => env('CLOUDINARY_API_KEY'),
 
-    'api_secret' => env('CLOUDINARY_SECRET'),
+    'api_secret' => env('CLOUDINARY_API_SECRET'),
 
 ];
