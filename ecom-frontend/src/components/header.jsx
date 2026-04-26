4e9743338b5742dc66aba2879f59a48aaa4c8560
Souvik Ghosh <personalpiku61140@gmail.com>
2026-04-27 01:18:12 +0530

    changing some navbar structure

--- ecom-frontend/src/components/header.jsx
+++ ecom-frontend/src/components/header.jsx
@@ -17,95 +17,91 @@ const totalItems = cartItems.reduce(
 
 
     return(
-
-        <main>
+      <main>
   <nav className="navbar navbar-expand-lg fixed-top">
     <div className="container-fluid">
+
+      {/* Brand */}
       <a className="navbar-brand" href="#">Brand Icon</a>
+
+      {/* Hamburger */}
       <button
         className="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        data-bs-target="#navbarNav"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        {/* Left side menu */}
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      {/* LEFT SIDE (only menu items inside collapse) */}
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav me-auto">
+
           <li className="nav-item">
-            <Link className="nav-link active" to="/">Home</Link>
+            <Link className="nav-link" to="/">Home</Link>
           </li>
 
-          {user?.role === "admin" &&(
-          <li className="nav-item dropdown">
-            <Link
-              className="nav-link dropdown-toggle"
-              to="/account"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Account
-            </Link>
-            <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" to="/profile">Profile</Link></li>
-              <li><Link className="dropdown-item" to="/register">New Register</Link></li>
-              <li><Link className="dropdown-item" to="/signin">Sign In</Link></li>
-              <li><hr className="dropdown-divider" /></li>
-              <li><Link className="dropdown-item" to="/admin">Admin</Link></li>
-            </ul>
-          </li>
+          {user?.role === "admin" && (
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle"
+                to="/account"
+                data-bs-toggle="dropdown"
+              >
+                Account
+              </Link>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" to="/profile">Profile</Link></li>
+                <li><Link className="dropdown-item" to="/register">New Register</Link></li>
+                <li><Link className="dropdown-item" to="/signin">Sign In</Link></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><Link className="dropdown-item" to="/admin">Admin</Link></li>
+              </ul>
+            </li>
           )}
+
           <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
+            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
               Categories
             </a>
             <ul className="dropdown-menu">
               <li><Link className="dropdown-item" to="/category/men">Men</Link></li>
               <li><Link className="dropdown-item" to="/category/women">Women</Link></li>
-              <li><Link className="dropdown-item" to="/categories/kids">Kids</Link></li>
+              <li><Link className="dropdown-item" to="/category/kids">Kids</Link></li>
               <li><Link className="dropdown-item" to="/others">Collections</Link></li>
             </ul>
           </li>
+
         </ul>
-
-        {/* Right side menu */}
-        <div className="d-flex align-items-center gap-3">
-          <div className="search-bar">
-            <SearchBar/>
-          </div>
-          {/* New icons */}
-          <Link to="/liked" className="btn btn-outline-dark position-relative">
-            <i className="bi bi-heart fs-5"></i>
-            {/* Optionally, add badge for liked items */}
-            {/* <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">3</span> */}
-          </Link>
-
-          <Link to="/account" className="btn btn-outline-dark">
-            <i className="bi bi-person fs-5"></i>
-          </Link>
-
-          <Link to="/cart" className="btn btn-outline-dark position-relative">
-            <i className="bi bi-cart fs-5"></i>
-            {totalItems > 0 && (
-              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                {totalItems}
-              </span>
-            )}
-          </Link>
-        </div>
       </div>
+
+      {/* RIGHT SIDE (ALWAYS visible) */}
+      <div className="d-flex align-items-center gap-3 ms-auto">
+
+        <div className="d-none d-md-block">
+          <SearchBar />
+        </div>
+
+        <Link to="/liked" className="btn btn-outline-dark position-relative">
+          <i className="bi bi-heart fs-5"></i>
+        </Link>
+
+        <Link to="/account" className="btn btn-outline-dark">
+          <i className="bi bi-person fs-5"></i>
+        </Link>
+
+        <Link to="/cart" className="btn btn-outline-dark position-relative">
+          <i className="bi bi-cart fs-5"></i>
+          {totalItems > 0 && (
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              {totalItems}
+            </span>
+          )}
+        </Link>
+
+      </div>
+
     </div>
   </nav>
 </main>
